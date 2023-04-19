b3ace0336efa3aa156a4d28a4f461b329c89952b
RohitMBhosale <bhosalerohit204@gmail.com>
2023-04-20 00:16:34 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <h1>Benifits of using React ...</h1>
+        <ol>
+          <li>Component based Architecture</li>
+          <li>Virtual DOM of efficient update</li>
+          <li>Rich ecosystem and community</li>
+          <li>Cross-Platform development</li>
+          <li>Strong Community Support</li>
+        </ol>
       </header>
     </div>
   );
