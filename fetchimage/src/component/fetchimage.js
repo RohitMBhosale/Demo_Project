89ebdc377ac4f36fc1da720796fa823aea8d1498
RohitMBhosale <bhosalerohit204@gmail.com>
2023-05-05 09:35:59 +0530

    button added to change the image

--- fetchimage/src/component/fetchimage.js
+++ fetchimage/src/component/fetchimage.js
@@ -1,23 +1,31 @@
-import React ,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './fetchimage.css'
+import axios from "axios"
 
-function Fetchimage()
-{
-    const [ image,setImage]=useState('')
+function Fetchimage() {
+  const [image, setImage] = useState('')
 
-    useEffect(()=>
-    {
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then((data)=> data.json())
-    .then ((response)=> setImage(response.message))
-    .catch((error) => console.log(error))  
-    },[])
+  function changeImage() {
 
-    return(
+    axios.get('https://dog.ceo/api/breeds/image/random')
+
+      .then((response) => setImage(response.data.message))
+      .catch((error) => console.log(error))
+
+  }
+  useEffect(() => {
+    changeImage();
+  }, [])
+
+  return (
+    <div>
       <div className='parent'>
-          <img src={image}/>
-      </div>
-    )
+      <img src={image} />
+      <button className='btn' onClick={changeImage}> Change Image</button>
+    </div>
+    </div>
+
+  )
 }
 
 export default Fetchimage;
