76058167ce9b8bc8a94cab78e1304f5b83847089
RohitMBhosale <bhosalerohit204@gmail.com>
2023-05-18 10:47:52 +0530

    making the quiz which is update the question automatically once answer is submitted

---
+++ quiztest/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QuizTest from './Components/Molecules/quiz';
+
+function App() {
+  return (
+    <div className="App">
+     <QuizTest/>
+    </div>
+  );
+}
+
+export default App;
